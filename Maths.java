84cd07fda3ef7067015952f803638f9aeb03a837
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-09-19 23:01:12 +0530

    Added count trailing zeros of factorial

--- Maths.java
+++ Maths.java
@@ -3,7 +3,8 @@ public class Maths{
 
 	public static void main(String args[]){
 		//System.out.println(countDigits(7));
-		System.out.println(isPalindrome(3634));
+		//System.out.println(isPalindrome(3634));
+		System.out.println(countTrailingZeros(10));
 	}
 
 	private static int countDigits(int n){
@@ -35,6 +36,19 @@ public class Maths{
 		}	
 
 		return rev == num;
+	}
+
+
+	private static int countTrailingZeros(int num){
+
+		int count = 0;
+
+		for(int i = 5; i <= num; i=i*5){
+			count += num / i;
+		}
+
+		return count;
+
 	}
 
 
