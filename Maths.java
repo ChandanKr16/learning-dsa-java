4ee7e202f26f1457ad7b371c2937fc5b203469a8
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-09-19 22:25:46 +0530

    Efficient Count digits method added

--- Maths.java
+++ Maths.java
@@ -2,11 +2,16 @@ public class Maths{
 
 
 	public static void main(String args[]){
-		System.out.println(countDigits(121));
+		System.out.println(countDigits(7));
 	}
 
 	private static int countDigits(int n){
-		int count = 0;
+
+		if(n == 0) return 1;
+		
+		return (int) (Math.log10(n) + 1);
+
+		/*int count = 0;
 
 		while(n != 0){
 			count++;
@@ -14,7 +19,12 @@ public class Maths{
 		}
 
 
-		return count;
+		return count;*/
+
 	}
+
+
+
+
 
 }
